9819723d3cce96f20adea98f8df578e2af3784fe
SagnikGos <sagnik.gos06@gmail.com>
2025-03-16 19:24:25 +0530

    bug

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,4 +1,4 @@
-const API_URL = "https://rr-coupon-2.onrender.com"; // Change this to your deployed backend URL
+const API_URL = "https://coupon-portal.onrender.com/"; // Change this to your deployed backend URL
 
 export const claimCoupon = async () => {
   try {
